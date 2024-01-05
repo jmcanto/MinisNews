fd5b9a7e0837b3da1670c26147402a98ae70ed45
José María Canto Ortiz <jmcanto@gmail.com>
2024-01-05 18:39:51 +0100

    corrección del formato del nombre del fichero de log

--- incl/funciones.php
+++ incl/funciones.php
@@ -3,7 +3,7 @@ class utilidades
 {
     public static function log($tipo, $texto)
     { 
-        $ficherolog = fopen('/var/log/minisnews/minisnews'.date("YMd").'.log','a'); 
+        $ficherolog = fopen('/var/log/minisnews/minisnews'.date("Ymd").'.log','a'); 
         fwrite($ficherolog, "[".date('Y-m-d h:i:s')."]\t$tipo\t$texto\r\n");
         fclose($ficherolog); 
     }
